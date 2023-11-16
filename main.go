38e80a53191651ad3f25fea323836f062a0f37fc
John Doe <mohamed.omrane.ensit@gmail.com>
2023-11-16 14:12:02 +0100

    Initial project version

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+
+	"estiam/dictionary"
+)
+
+func main() {
+	// bufio.NewReader(os.Stdin)
+	fmt.Println("Hello there")
+}
+
+func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+
+}
+
+func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
+
+}
+
+func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
+
+}
+
+func actionList(d *dictionary.Dictionary) {
+
+}
