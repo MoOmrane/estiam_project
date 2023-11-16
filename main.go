2b78fe24a8ea3cb3d53ade111ff829b8bec9a222
MoOmrane <mohamed.omrane.ensit@gmail.com>
2023-11-16 16:00:55 +0100

    Ajout de gitignore et complement des fonctions pour remove define list

--- main.go
+++ main.go
@@ -1,29 +1,49 @@
-package main
+	package main
 
-import (
-	"fmt"
-	"bufio"
+	import (
+		"fmt"
+		"bufio"
 
-	"estiam/dictionary"
-)
+		"estiam/dictionary"
+	)
 
-func main() {
-	// bufio.NewReader(os.Stdin)
-	fmt.Println("Hello there")
-}
+	func main() {
+		// bufio.NewReader(os.Stdin)
+		fmt.Println("Hello there")
+	}
 
-func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+	func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
+		fmt.Print("Mot : ")
+		word, _ := reader.ReadString('\n')
+		fmt.Print("Définition : ")
+		definition, _ := reader.ReadString('\n')
+		d.Add(strings.TrimSpace(word), strings.TrimSpace(definition))
+	}
+		
 
-}
+	func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
+		fmt.Print("Mot à définir : ")
+		word, _ := reader.ReadString('\n')
+		entry, err := d.Get(strings.TrimSpace(word))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Définition de '%s' : %s\n", word, entry.String())
+	}
 
-func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
+	func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
+		fmt.Print("Mot à supprimer : ")
+		word, _ := reader.ReadString('\n')
+		d.Remove(strings.TrimSpace(word))
+		fmt.Printf("'%s' a été supprimé du dictionnaire.\n", strings.TrimSpace(word))
+	}	
 
-}
-
-func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
-
-}
-
-func actionList(d *dictionary.Dictionary) {
-
-}
+	func actionList(d *dictionary.Dictionary) {
+		words, entries := d.List()
+		fmt.Println("Mots dans le dictionnaire :")
+		for _, word := range words {
+			fmt.Printf("%s: %s\n", word, entries[word].String())
+		}
+	}
+	
